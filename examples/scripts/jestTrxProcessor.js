dcc4a91c847240de14b585f6e5ece8b10c9135e9
Dan Homola <dan.homola@hotmail.cz>
2016-10-16 09:31:39 +0200

    Improve exmaples

--- examples/scripts/jestTrxProcessor.js
+++ examples/scripts/jestTrxProcessor.js
@@ -1,7 +1,7 @@
 var builder = require('jest-trx-results-processor');
 
 var processor = builder({
-    outputFile: 'configurable.trx'
+    outputFile: 'relative/path/to/resulting.trx'
 });
 
 module.exports = processor;
