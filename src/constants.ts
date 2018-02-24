1a9cf27fe984441f392df93785dc8a369a4679e1
Dan Homola <dan.homola@hotmail.cz>
2018-02-24 21:26:19 +0100

    style: split the render function and the trx-generator file

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+// Magic GUID's and other constants
+export const testListNotInListId = "8c84fa94-04c1-424b-9868-57a2d4851a1d";
+export const testListAllLoadedResultsId = "19431567-8539-422a-85d7-44ee4e166bda";
+export const testType = "13cdc9d9-ddb5-4fa4-a97d-d965ccfc6d4b";
+
+export const testOutcomeTable: { [outcome: string]: string } = {
+  failed: "Failed",
+  passed: "Passed",
+  pending: "NotExecuted",
+};
--- src/trx-generator.ts
+++ src/trx-generator.ts
@@ -1,88 +1,37 @@
 import * as os from "os";
 import * as uuid from "uuid";
-import * as builder from "xmlbuilder";
+import {
+  create as createXmlBuilder,
+  XMLDocType,
+  XMLElementOrXMLNode,
+} from "xmlbuilder";
 
+import {
+  testListAllLoadedResultsId,
+  testListNotInListId,
+  testOutcomeTable,
+  testType,
+} from "./constants";
 import {
   JestTestResult,
   JestTestRunResult,
   JestTestSuiteResult,
 } from "./types";
+import {
+  formatDuration,
+  getFullTestName,
+  getSuitePerTestDuration,
+  getTestClassName,
+  sanitizeString,
+} from "./utils";
 
-// Magic GUID's and other constants
-const testListNotInListId = "8c84fa94-04c1-424b-9868-57a2d4851a1d";
-const testListAllLoadedResultsId = "19431567-8539-422a-85d7-44ee4e166bda";
-const testType = "13cdc9d9-ddb5-4fa4-a97d-d965ccfc6d4b";
-
-const testOutcomeTable: { [outcome: string]: string } = {
-  failed: "Failed",
-  passed: "Passed",
-  pending: "NotExecuted",
-};
-
-// Auxilliary functions
-const getFullTestName = (testResult: JestTestResult): string =>
-  testResult.ancestorTitles && testResult.ancestorTitles.length
-    ? `${testResult.ancestorTitles.join(" / ")} / ${testResult.title}`
-    : testResult.title;
-
-const getTestClassName = (testResult: JestTestResult): string =>
-  testResult.ancestorTitles && testResult.ancestorTitles.length
-    ? testResult.ancestorTitles[0]
-    : "No suite";
-
-const getSuitePerTestDuration = (
-  testSuiteResult: JestTestSuiteResult,
-): number =>
-  // take the total duration of suite and divide it by the number of tests
-  // (Jest does not provide per test performance info)
-  Math.floor(
-    (testSuiteResult.perfStats.end - testSuiteResult.perfStats.start) /
-      (testSuiteResult.numPassingTests +
-        testSuiteResult.numFailingTests +
-        testSuiteResult.numPendingTests),
-  );
-
-// Adapted from https://github.com/hatchteam/karma-trx-reporter
-const formatDuration = (duration: number): string => {
-  let durationInner = duration || 0;
-  const ms = durationInner % 1000;
-  durationInner -= ms;
-  const s = (durationInner / 1000) % 60;
-  durationInner -= s * 1000;
-  const m = (durationInner / 60000) % 60;
-  durationInner -= m * 60000;
-  const h = (durationInner / 3600000) % 24;
-  durationInner -= h * 3600000;
-  const d = durationInner / 86400000;
-
-  return (
-    (d > 0 ? d + "." : "") +
-    (h < 10 ? "0" + h : h) +
-    ":" +
-    (m < 10 ? "0" + m : m) +
-    ":" +
-    (s < 10 ? "0" + s : s) +
-    "." +
-    (ms < 10 ? "00" + ms : ms < 100 ? "0" + ms : ms)
-  );
-};
-
-const sanitizationRegex = /[\u0000-\u0008\u000B-\u000C\u000E-\u001F\uD800-\uDFFF\uFFFE-\uFFFF]/g;
-const sanitizeString = (str: string): string =>
-  str && str.replace(sanitizationRegex, ""); // removes the characters that make xmlbuilder throw
-
-export const generateTrx = (testRunResult: JestTestRunResult): string => {
-  const computerName = os.hostname();
-  const userName =
-    process.env.SUDO_USER ||
-    process.env.LOGNAME ||
-    process.env.USER ||
-    process.env.LNAME ||
-    process.env.USERNAME;
-
-  // TestRun
-  const resultBuilder = builder
-    .create("TestRun", { version: "1.0", encoding: "UTF-8" })
+const renderTestRun = (
+  builder: XMLElementOrXMLNode,
+  testRunResult: JestTestRunResult,
+  computerName: string,
+  userName: string,
+) =>
+  builder
     .att("id", uuid.v4())
     .att(
       "name",
@@ -93,22 +42,29 @@ export const generateTrx = (testRunResult: JestTestRunResult): string => {
     .att("runUser", userName)
     .att("xmlns", "http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
 
-  // TestSettings
-  resultBuilder
+const renderTestSettings = (builder: XMLElementOrXMLNode) =>
+  builder
     .ele("TestSettings")
     .att("name", "Jest test run")
     .att("id", uuid.v4());
 
-  // Times
+const renderTimes = (
+  builder: XMLElementOrXMLNode,
+  testRunResult: JestTestRunResult,
+) => {
   const startTime = new Date(testRunResult.startTime).toISOString();
-  resultBuilder
+  builder
     .ele("Times")
     .att("creation", startTime)
     .att("queuing", startTime)
     .att("start", startTime);
+};
 
-  // ResultSummary
-  const summary = resultBuilder
+const renderResultSummary = (
+  builder: XMLElementOrXMLNode,
+  testRunResult: JestTestRunResult,
+) => {
+  const summary = builder
     .ele("ResultSummary")
     .att("outcome", testRunResult.success ? "Passed" : "Failed");
   summary
@@ -121,10 +77,10 @@ export const generateTrx = (testRunResult: JestTestRunResult): string => {
     .att("passed", testRunResult.numPassedTests)
     .att("failed", testRunResult.numFailedTests)
     .att("error", 0);
+};
 
-  // TestDefinitions
-  const testDefinitions = resultBuilder.ele("TestDefinitions");
-  const testLists = resultBuilder.ele("TestLists");
+const renderTestLists = (builder: XMLElementOrXMLNode) => {
+  const testLists = builder.ele("TestLists");
   testLists
     .ele("TestList")
     .att("name", "Results Not in a List")
@@ -133,72 +89,113 @@ export const generateTrx = (testRunResult: JestTestRunResult): string => {
     .ele("TestList")
     .att("name", "All Loaded Results")
     .att("id", testListAllLoadedResultsId);
+};
+
+const renderTestSuiteResult = (
+  testSuiteResult: JestTestSuiteResult,
+  testDefinitionsNode: XMLElementOrXMLNode,
+  testEntriesNode: XMLElementOrXMLNode,
+  resultsNode: XMLElementOrXMLNode,
+  computerName: string,
+) => {
+  const perTestDuration = getSuitePerTestDuration(testSuiteResult);
+  const perTestDurationFormatted = formatDuration(perTestDuration);
+
+  testSuiteResult.testResults.forEach((testResult, index) => {
+    const testId = uuid.v4();
+    const executionId = uuid.v4();
+    const fullTestName = getFullTestName(testResult);
+
+    // UnitTest
+    const unitTest = testDefinitionsNode
+      .ele("UnitTest")
+      .att("name", fullTestName)
+      .att("id", testId);
+    unitTest.ele("Execution").att("id", executionId);
+    unitTest
+      .ele("TestMethod")
+      .att("codeBase", `Jest_${fullTestName}`)
+      .att("name", fullTestName)
+      .att("className", getTestClassName(testResult));
+
+    // TestEntry
+    testEntriesNode
+      .ele("TestEntry")
+      .att("testId", testId)
+      .att("executionId", executionId)
+      .att("testListId", testListNotInListId);
+
+    // UnitTestResult
+    const result = resultsNode
+      .ele("UnitTestResult")
+      .att("testId", testId)
+      .att("executionId", executionId)
+      .att("testName", fullTestName)
+      .att("computerName", computerName)
+      .att("duration", perTestDurationFormatted)
+      .att(
+        "startTime",
+        new Date(
+          testSuiteResult.perfStats.start + index * perTestDuration,
+        ).toISOString(),
+      )
+      .att(
+        "endTime",
+        new Date(
+          testSuiteResult.perfStats.start + (index + 1) * perTestDuration,
+        ).toISOString(),
+      )
+      .att("testType", testType)
+      .att("outcome", testOutcomeTable[testResult.status])
+      .att("testListId", testListNotInListId);
+
+    if (testResult.status === "failed") {
+      result
+        .ele("Output")
+        .ele("ErrorInfo")
+        .ele("Message", sanitizeString(testResult.failureMessages.join("\n")));
+    }
+  });
+};
+
+export const generateTrx = (testRunResult: JestTestRunResult): string => {
+  const computerName = os.hostname();
+  const userName =
+    process.env.SUDO_USER ||
+    process.env.LOGNAME ||
+    process.env.USER ||
+    process.env.LNAME ||
+    process.env.USERNAME;
+
+  const resultBuilder = createXmlBuilder("TestRun", {
+    version: "1.0",
+    encoding: "UTF-8",
+  });
+
+  renderTestRun(resultBuilder, testRunResult, computerName, userName);
+
+  renderTestSettings(resultBuilder);
+
+  renderTimes(resultBuilder, testRunResult);
+
+  renderResultSummary(resultBuilder, testRunResult);
+
+  const testDefinitions = resultBuilder.ele("TestDefinitions");
+
+  renderTestLists(resultBuilder);
+
   const testEntries = resultBuilder.ele("TestEntries");
   const results = resultBuilder.ele("Results");
 
-  testRunResult.testResults.forEach(testSuiteResult => {
-    const perTestDuration = getSuitePerTestDuration(testSuiteResult);
-    const perTestDurationFormatted = formatDuration(perTestDuration);
-
-    testSuiteResult.testResults.forEach((testResult, index) => {
-      const testId = uuid.v4();
-      const executionId = uuid.v4();
-      const fullTestName = getFullTestName(testResult);
-
-      // UnitTest
-      const unitTest = testDefinitions
-        .ele("UnitTest")
-        .att("name", fullTestName)
-        .att("id", testId);
-      unitTest.ele("Execution").att("id", executionId);
-      unitTest
-        .ele("TestMethod")
-        .att("codeBase", `Jest_${fullTestName}`)
-        .att("name", fullTestName)
-        .att("className", getTestClassName(testResult));
-
-      // TestEntry
-      testEntries
-        .ele("TestEntry")
-        .att("testId", testId)
-        .att("executionId", executionId)
-        .att("testListId", testListNotInListId);
-
-      // UnitTestResult
-      const result = results
-        .ele("UnitTestResult")
-        .att("testId", testId)
-        .att("executionId", executionId)
-        .att("testName", fullTestName)
-        .att("computerName", computerName)
-        .att("duration", perTestDurationFormatted)
-        .att(
-          "startTime",
-          new Date(
-            testSuiteResult.perfStats.start + index * perTestDuration,
-          ).toISOString(),
-        )
-        .att(
-          "endTime",
-          new Date(
-            testSuiteResult.perfStats.start + (index + 1) * perTestDuration,
-          ).toISOString(),
-        )
-        .att("testType", testType)
-        .att("outcome", testOutcomeTable[testResult.status])
-        .att("testListId", testListNotInListId);
-
-      if (testResult.status === "failed") {
-        result
-          .ele("Output")
-          .ele("ErrorInfo")
-          .ele(
-            "Message",
-            sanitizeString(testResult.failureMessages.join("\n")),
-          );
-      }
-    });
-  });
+  testRunResult.testResults.forEach(testSuiteResult =>
+    renderTestSuiteResult(
+      testSuiteResult,
+      testDefinitions,
+      testEntries,
+      results,
+      computerName,
+    ),
+  );
 
   return resultBuilder.end({ pretty: true });
 };
