0cc02e3ca17efecd556592ab2a05741ae98594e7
Dan Homola <dan.homola@hotmail.cz>
2019-09-23 12:18:04 +0200

    fix: create output directory if it does not exist
    Fixes #89

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { AggregatedResult } from "@jest/test-result";
 import { Config } from "@jest/types";
 import { writeFileSync } from "fs";
+import mkdirp from "mkdirp";
+import path from "path";
+
 import { generateTrx, IOptions } from "./trx-generator";
 
 class TrxReporter {
@@ -17,6 +20,9 @@ class TrxReporter {
     aggregatedResults: AggregatedResult,
   ): Promise<void> | void => {
     const trx = generateTrx(aggregatedResults, this.options);
+
+    const targetDir = path.dirname(path.resolve(this.options.outputFile));
+    mkdirp.sync(targetDir);
 
     writeFileSync(this.options.outputFile, trx, { encoding: "utf8" });
     process.stdout.write("DONE\n");
